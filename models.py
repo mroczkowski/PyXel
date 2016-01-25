2f28e2f2388991b96b5b1d27d57d38a218b36a74
gogrean <gogrean@cfa.harvard.edu>
2016-01-25 12:32:12 -0800

    Added constant model.

--- models.py
+++ models.py
@@ -3,6 +3,10 @@
 # Created on 03.22.2015
 #
 
+def const(c):
+    """Fit a constant to the data."""
+    return c
+
 def beta(r, beta, rc, s0, bkg):
    """Fit beta-model to the data."""
    mod = bkg + s0 * (1. + (r/rc)**2.) ** (-3.*beta+0.5)
