bbd5449cfe2add5b1d2488558436ca1684d2a0a4
Georgiana Ogrean <georgiana.ogrean@gmail.com>
2016-04-04 08:54:51 -0700

    Integration option implemented, mostly so that when fitting a broken power-law model the jump location is not always at the center of a bin. The implementation is a little clumsy to use atm, but otherwise seems to work correctly.

--- models.py
+++ models.py
@@ -1,6 +1,56 @@
 import numpy as np
 import scipy.integrate
 from astropy.modeling import Fittable1DModel, Parameter
+from scipy import integrate
+
+# class IntModel(Fittable1DModel):
+#
+#     r = Parameter(default = 0., min = 0.)
+#     r_widths = Parameter(default = 0., min = 0.)
+#     model = Parameter() # ???
+#
+#     def __init__(self, radius=r.default, widths=r_widths.default, model=model.default):
+#         if all(r_width is None for r_width in np.array(widths)):
+#             widths = r * 0.
+#         super(IntModel, self).__init__(radius=radius, widths=widths, model=model)
+#
+#     @staticmethod
+#     def evaluate(x, *params):
+#         params = model.parameters
+#         result = np.array([int.quad(model.evaluate, d - dw, d + dw, params)[0]
+#                            / (2.*dw) for d, dw in zip(r, r_widths)])
+#         return result
+#
+#     @staticmethod
+#     def fit_deriv(r, r_widths, model):
+#         params = model.parameters
+#         result = np.array([int.quad(model.fit_deriv, d - dw, d + dw, params)[0]
+#                            / (2.*dw) for d, dw in zip(r, r_widths)])
+#         return result
+
+def IntModel(model_cls):
+    class MyIntModel(model_cls):
+        def __init__(self, widths, *args, **kwargs):
+            self._widths = widths
+            super(MyIntModel, self).__init__(*args, **kwargs)
+
+        def evaluate(self, x, *params):
+            fn = super(MyIntModel, self).evaluate
+
+            result = np.array([integrate.quad(lambda r: fn(r, *params), d - dw, d + dw)[0] / (2.*dw)
+                               for d, dw in zip(x, self._widths)])
+            return result
+
+        def fit_deriv(self, x, *params):
+            fn = super(MyIntModel, self).fit_deriv
+
+            result = [np.array([integrate.quad(lambda r: fn(r, *params)[i], d - dw, d + dw)[0] / (2.*dw)
+                                for d, dw in zip(x, self._widths)])
+                      for i in range(len(params))]
+
+            return result
+
+    return MyIntModel
 
 class Beta(Fittable1DModel):
     s0 = Parameter(default = 1e-2, min = 1e-12)
@@ -11,18 +61,16 @@ class Beta(Fittable1DModel):
     @staticmethod
     def evaluate(x, s0, beta, rc, const):
         result = s0 * (1. + (x/rc)**2) ** (0.5 - 3*beta) + const
-        print(result)
         return result
 
     @staticmethod
     def fit_deriv(x, s0, beta, rc, const):
-        print('deriv...')
         d_s0 = (1. + (x/rc)**2) ** (0.5 - 3*beta)
         d_beta = -3 * s0 * np.log(1. + (x/rc)**2) * \
                  (1. + (x/rc)**2) ** (0.5 - 3*beta)
         d_rc = -2 * s0 * x**2 * (0.5 - 3*beta) / rc**3 \
                * (1. + (x/rc)**2) ** (-0.5 - 3*beta)
-        return [d_s0, d_beta, d_rc, list(np.ones(len(x)))]
+        return [d_s0, d_beta, d_rc, np.ones_like(x)]
 
 class BrokenPow(Fittable1DModel):
     ind1 = Parameter(default = 0.)
@@ -34,22 +82,81 @@ class BrokenPow(Fittable1DModel):
 
     @staticmethod
     def evaluate(x, ind1, ind2, norm, rbreak, jump, const):
-        norm_after_jump = norm / jump**2
-        sx = np.zeros(len(x))
-        for i in range(len(x)):
-            xval = x[i]
-            if xval <= rbreak:
-                sx[i] = norm * scipy.integrate.quad(lambda z:
-                                   ((xval**2 + z**2) / rbreak**2)**(-ind1),
-                                   1e-4, np.sqrt(rbreak**2 - xval**2))[0] + \
-                        norm_after_jump * scipy.integrate.quad(lambda z:
-                                   ((xval**2 + z**2) / rbreak**2)**(-ind2),
-                                   np.sqrt(rbreak**2 - xval**2), 1e4)[0]
-            else:
-                sx[i] = norm_after_jump * scipy.integrate.quad(lambda z:
-                                   ((xval**2 + z**2) / rbreak**2)**(-ind2),
-                                    1e-4, 1e4)[0]
+        if not isinstance(x, (int, float)):
+            sx = np.zeros_like(x)
+            for i in range(len(sx)):
+                sx[i] = BrokenPow.evaluate_one(x[i], ind1, ind2, norm, rbreak, jump)
+        else:
+            sx = BrokenPow.evaluate_one(x, ind1, ind2, norm, rbreak, jump)
+
         return sx+const
 
-#    @staticmethod
-#    def fit_deriv(x, ind1, ind2, norm, rbreak, jump):
+    @staticmethod
+    def evaluate_one(xval, ind1, ind2, norm, rbreak, jump):
+        norm_after_jump = norm / jump**2
+        if xval <= rbreak:
+            return norm * scipy.integrate.quad(lambda z:
+                               ((xval**2 + z**2) / rbreak**2)**(-ind1),
+                               1e-4, np.sqrt(rbreak**2 - xval**2))[0] + \
+                    norm_after_jump * scipy.integrate.quad(lambda z:
+                               ((xval**2 + z**2) / rbreak**2)**(-ind2),
+                               np.sqrt(rbreak**2 - xval**2), 1e4)[0]
+        else:
+            return norm_after_jump * scipy.integrate.quad(lambda z:
+                               ((xval**2 + z**2) / rbreak**2)**(-ind2),
+                                1e-4, 1e4)[0]
+
+    @staticmethod
+    def fit_deriv(xval, ind1, ind2, norm, rbreak, jump, const):
+        fn1 = lambda z: ((xval**2 + z**2) / rbreak**2)**(-ind1)
+        fn2 = lambda z: ((xval**2 + z**2) / rbreak**2)**(-ind2)
+        norm_after_jump = norm / jump**2
+        if xval <= rbreak:
+            d_ind1 = -norm * scipy.integrate.quad(lambda z: fn1(z) *
+                         np.log((xval**2 + z**2)/rbreak**2),
+                         1e-4, np.sqrt(rbreak**2 - xval**2))[0]
+
+            d_ind2 = -norm_after_jump * scipy.integrate.quad(lambda z: fn2(z) *
+                         np.log((xval**2 + z**2)/rbreak**2),
+                         np.sqrt(rbreak**2 - xval**2), 1e4)[0]
+
+            d_norm = scipy.integrate.quad(lambda z:
+                           ((xval**2 + z**2) / rbreak**2)**(-ind1),
+                           1e-4, np.sqrt(rbreak**2 - xval**2))[0] + \
+                     1./jump**2 * scipy.integrate.quad(lambda z:
+                            ((xval**2 + z**2) / rbreak**2)**(-ind2),
+                            np.sqrt(rbreak**2 - xval**2), 1e4)[0]
+
+            d_rbreak = norm * (2. * ind1 * scipy.integrate.quad(lambda z:
+                                    fn1(z) / rbreak,
+                                    1e-4, np.sqrt(rbreak**2 - xval**2))[0] +
+                       rbreak / np.sqrt(rbreak**2 - xval**2)) + \
+                       norm_after_jump * (2. * ind2 * scipy.integrate.quad(
+                                    lambda z: fn2(z) / rbreak,
+                                    np.sqrt(rbreak**2 - xval**2), 1e4)[0] -
+                       rbreak / np.sqrt(rbreak**2 - xval**2))
+
+            d_jump = -2*norm / jump**3 * scipy.integrate.quad(lambda z:
+                         ((xval**2 + z**2) / rbreak**2)**(-ind2),
+                         np.sqrt(rbreak**2 - xval**2), 1e4)[0]
+
+        else:
+            d_ind1 = np.zeros_like(xval)
+
+            d_ind2 = -norm_after_jump * scipy.integrate.quad(lambda z: fn2(z) *
+                         np.log((xval**2 + z**2)/rbreak**2),
+                         1e-4, 1e4)[0]
+
+            d_norm = 1/jump**2 * scipy.integrate.quad(lambda z:
+                               ((xval**2 + z**2) / rbreak**2)**(-ind2),
+                                1e-4, 1e4)[0]
+
+            d_rbreak = 2. * norm_after_jump * ind2 * scipy.integrate.quad(
+                         lambda z: fn2(z) / rbreak, 1e-4, 1e4)[0]
+
+            d_jump = -2*norm / jump**3 * scipy.integrate.quad(lambda z:
+                         ((xval**2 + z**2) / rbreak**2)**(-ind2),
+                         1e-4, 1e4)[0]
+
+        d_const = np.ones_like(xval)
+        return [d_ind1, d_ind2, d_norm, d_rbreak, d_jump, d_const]
--- test_code.py
+++ test_code.py
@@ -1,17 +1,16 @@
-import pyfits
-import numpy as np
-from load_data import Image, load_region
-import matplotlib.pyplot as plt
-#from profile import Box #, counts_profile
-
 import os
 import pickle
+import numpy as np
 
-from astropy.modeling.functional_models import Const1D
-from modelsastropy import Beta, BrokenPow
+import matplotlib.pyplot as plt
 from astropy.modeling.fitting import LevMarLSQFitter
 
-pkl = 'profile-core-circ.pkl'
+from load_data import Image, load_region
+from models import Beta, BrokenPow, IntModel
+
+#from astropy.modeling.functional_models import Const1D
+
+pkl = 'profile-core-ell.pkl'
 
 if os.path.exists(pkl):
     with open(pkl, 'rb') as f:
@@ -21,56 +20,33 @@ else:
     src_img = Image("srcfree_bin4_500-4000_band1_thresh.img")
     exp_img = Image("srcfree_bin4_500-4000_thresh.expmap_nosrcedg")
     bkg_img = Image("srcfree_bin4_500-4000_bgstow_renorm.img")
+    bkg_err_img = Image("srcfree_bin4_500-4000_bgstow_err_renorm.img")
 
     # Read the region file in which the surface brightness profile will be created.
-    region = load_region("core_sx_circ.reg")
+    region = load_region("core_sx_ell.reg")
 
     # Create the profile and bin it to a minimum of 100 counts/bin
-    p = region.sb_profile(src_img, bkg_img, exp_img, min_counts=30, islog=False) 
+    p = region.sb_profile(src_img, bkg_img, bkg_err_img, exp_img,
+                          min_counts=30, islog=False)
     with open(pkl, 'wb') as f:
         pickle.dump(p, f)
 
 rmin = 0.1
-rmax = 5.0
+rmax = 1.0
 r = np.array([pp[0] for pp in p if rmax >= pp[0] >= rmin])
 r_err = np.array([pp[1] for pp in p if rmax >= pp[0] >= rmin])
+bkg = np.array([pp[5] for pp in p if rmax >= pp[0] >= rmin])
+bkg_err = np.array([pp[6] for pp in p if rmax >= pp[0] >= rmin])
 sx = np.array([pp[7] for pp in p if rmax >= pp[0] >= rmin])
 sx_err = np.array([pp[8] for pp in p if rmax >= pp[0] >= rmin])
 
-mod = BrokenPow(ind1=0.5, ind2=1.2, norm=1e-4, rbreak=0.35, jump=2.0, const=9e-7)
-#mod = Const1D(amplitude=1e-6)
+#mod = Beta(s0=1e-3, beta=0.7, rc=0.25, const=9e-7)
+mod = BrokenPow(ind1=0.5, ind2=1.2, norm=1e-4, rbreak=0.322, jump=2.0, const=1.30203247569e-06)
 mod.const.fixed = True
-mod.rbreak.fixed = True
+intmod = IntModel(radius=r, widths=r_err, model=mod)
 
 fit = LevMarLSQFitter()
-m = fit(mod, r, sx, weights=1./sx_err, estimate_jacobian=True, maxiter=500) 
+m = fit(intmod, r, sx, weights=1./sx_err, estimate_jacobian=True, maxiter=500)
 print(m)
 print(fit.fit_info)
-
-plt.figure(figsize=(8,5))
-plt.scatter(r, sx, c="#1e8f1e", alpha=0.85, s=35, marker="s") 
-plt.errorbar(r, sx, xerr=r_err, yerr=sx_err,
-             linestyle="None", color="#1e8f1e")
-plt.plot(r, m(r), color="#ffa500", linewidth=2, alpha=0.75)
-plt.semilogx()
-plt.semilogy()
-plt.xlim(0.1, 8.0) 
-plt.ylim(1e-7, 1e-3)
-plt.savefig('blah.eps')
-
-
-
-
-# mod = Beta()
-# mod.set_parameter('beta', 0.6)
-# mod.set_parameter('rc', 0.9)
-# mod.set_parameter('s0', 3.7e-4, max_bound = 1e-2)
-# mod.set_parameter('const', 3e-8)
-# print(mod.fit(p, statistics='cash'))
-# print(mod.params)
-#
-#
-# mod = Model("beta") + Model("const")
-# params = mod.set_params(beta=0.7, rc=0.4, s0=1e-4, const=3e-6)
-#
-# mod = Beta()
+print(m.ind1, m.ind2, m.norm, m.rbreak, m.jump)
