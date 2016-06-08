Fixed typos/bugs introduced by the change in the surface brightness calculation.

prof.py
@@ -75,6 +75,8 @@ class Region(object):
         raw_rate, net_rate, bkg_rate = 0., 0., 0.
         err_raw_rate_sq, err_net_rate_sq, err_bkg_rate_sq = 0., 0., 0.
 
+        exp_raw = 0.
+        exp_bkg = 0.
         for i in range(len(counts_img_data)):
             if not bkg_corr[i]:
                 if not 'BKGNORM' in bkg_img_hdr[i]:
@@ -97,7 +99,7 @@ class Region(object):
                 raw_cts += counts_img_data[i][j, k]
                 bkg_cts += bkg_img_data[i][j, k]
                 exp_raw += exp_val
-            exp_bkg += exp_raw / bkg_corr_i
+                exp_bkg += exp_val_bkg / bkgnorm
             net_cts = raw_cts - bkg_cts * bkg_corr_i
         if only_net_cts:
             return net_cts
@@ -107,7 +109,7 @@ class Region(object):
         err_raw_rate = np.sqrt(raw_cts) / exp_raw
         err_bkg_rate = np.sqrt(bkg_cts) / exp_bkg
         err_net_rate = np.sqrt(raw_cts / exp_raw**2 + bkg_cts / exp_bkg**2)
-        return raw_cts, net_cts, bkg_cts, 
+        return raw_cts, net_cts, bkg_cts, \
                raw_rate, err_raw_rate, net_rate, err_net_rate, bkg_rate, err_bkg_rate
 
     def merge_bins(self, counts_img, bkg_img, exp_img,
@@ -197,15 +199,15 @@ class Region(object):
 
         profile = []
         for current_bin in bins:
-            raw_cts, net_cts, bkg_cts, 
-                raw_rate, err_raw_rate, net_rate, err_net_rate, 
+            raw_cts, net_cts, bkg_cts, \
+                raw_rate, err_raw_rate, net_rate, err_net_rate, \
                 bkg_rate, err_bkg_rate = \
                     self.get_bin_vals(counts_img, bkg_img, exp_img,
                                       current_bin[2])
             bin_radius = (current_bin[0] + current_bin[1]) / 2.
             bin_width = current_bin[1] - bin_radius
-            bin_values = (bin_radius, bin_width, raw_cts, err_raw_cts, 
-                          net_cts, err_net_cts, bkg_cts, err_bkg_cts, 
+            bin_values = (bin_radius, bin_width, raw_cts, 
+                          net_cts, bkg_cts, 
                           raw_rate, err_raw_rate, net_rate, err_net_rate,
                           bkg_rate, err_bkg_rate)
             bin_values = bin_pix2arcmin(bin_values, pix2arcmin)